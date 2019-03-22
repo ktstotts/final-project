a3660abe66170096272aab13cd2bd1e4160465ff
Katie Stotts <ktstotts@gmail.com>
2019-03-22 17:37:34 -0400

    added jquery function and fixed script tag

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,1 +1,33 @@
-$('.bg-modal').hide();
+// document.querySelector('button').addEventListener('click',
+// function() {
+//    document.querySelector('.modal-popup').style.display = 'none';
+// });
+// //
+// alert('hello');
+
+
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $(".modal-popup").hide();
+//     $(".modal-popup").fadeIn("slow");
+//     $(".modal-popup").fadeIn(3000);
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $(".logo").click(function(){
+//     $(this).hide();
+//   });
+// });
+// $(document).ready(function(){
+//   $(".logo").click(function(){
+//     $("img").fadeIn();
+//     $("img").fadeIn("slow");
+//     $("img").fadeIn(3000);
+//   });
+// });
+$(document).ready(function(){
+  $("button").click(function(){
+    $('.modal-popup').hide();
+  });
+});
