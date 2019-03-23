6a8bde9fee78f38c31bb363c135bffbe156cbf03
Katie Stotts <ktstotts@gmail.com>
2019-03-22 20:09:28 -0400

    added loading page

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,33 +1,27 @@
-// document.querySelector('button').addEventListener('click',
-// function() {
-//    document.querySelector('.modal-popup').style.display = 'none';
-// });
-// //
-// alert('hello');
 
 
-// $(document).ready(function(){
-//   $("button").click(function(){
-//     $(".modal-popup").hide();
-//     $(".modal-popup").fadeIn("slow");
-//     $(".modal-popup").fadeIn(3000);
-//   });
-// });
-//
-// $(document).ready(function(){
-//   $(".logo").click(function(){
-//     $(this).hide();
-//   });
-// });
-// $(document).ready(function(){
-//   $(".logo").click(function(){
-//     $("img").fadeIn();
-//     $("img").fadeIn("slow");
-//     $("img").fadeIn(3000);
-//   });
-// });
+//loading screen style//
+
+
+$(document).ready(function(){
+  $('.load').fadeIn(5000);
+  $('.load').delay(3000);
+  $('.load').fadeOut();
+  });
+
+
+//modal style//
+
+
+
 $(document).ready(function(){
   $("button").click(function(){
+    $('.modal-popup').fadeOut();
+  });
+});
+
+$(document).ready(function(){
+  $(".close").click(function(){
     $('.modal-popup').hide();
   });
 });
